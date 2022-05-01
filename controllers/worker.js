e7f0169f9f2ad5dfafe4d291edcf17260566f869
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-05-01 14:17:30 +0530

    delete edit added

--- controllers/worker.js
+++ controllers/worker.js
@@ -1,10 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
 import WorkerModal from "../models/worker.js";
 
 export const createWorker = async (req, res) => {
     const worker = req.body;
     const newWorker = new WorkerModal({
         ...worker,
+        creator: req.userId,
         createdAt: new Date().toISOString(),
     });
 
@@ -26,3 +28,53 @@ export const getWorkers = async (req, res) => {
         res.status(404).json({ message: "Something went wrong" })
     }
 }
+
+
+export const getWorkersByUser = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: "User does not exist" })
+    }
+
+    const userWorkers = await WorkerModal.find({ creator: id });
+    res.status(200).json(userWorkers);
+
+};
+
+export const deleteWorker = async (req, res) => {
+    const { id } = req.params;
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: `No Task exist with id: ${id}`})
+    }
+    await WorkerModal.findByIdAndRemove(id);
+    res.json({message: "Task Deleted Succesfully"})
+    }
+    catch (error) {
+        res.status(404).json({ message: "Something went wrong" });
+    }
+}
+
+
+export const updatedWorker = async (req, res) => {
+    const { id } = req.params;
+    const {title, description, creator, imageFile, tags} = req.body;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ message: `No Task exist with id: ${id}` });
+      }
+      
+      const updatedWorker = {
+          creator,
+          title,
+          description,
+          tags,
+          imageFile,
+          _id: id
+      }
+    await WorkerModal.findByIdAndUpdate(id, updatedWorker, {new: true});
+    res.json(updatedWorker);
+  } catch (error) {
+    res.status(404).json({ message: "Something went wrong" });
+  }
+};
--- routes/worker.js
+++ routes/worker.js
@@ -1,10 +1,20 @@
 import express from "express";
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
-import { createWorker, getWorkers } from "../controllers/worker.js";
+import {
+  createWorker,
+  getWorkers,
+  getWorkersByUser,
+  updatedWorker,
+  deleteWorker,
+} from "../controllers/worker.js";
 
-router.post("/", createWorker);
+router.post("/", auth, createWorker);
 router.get("/", getWorkers);
+router.delete("/:id", auth, deleteWorker);
+router.patch("/:id", auth, updatedWorker);
+router.get("/userWorkers/:id", auth, getWorkersByUser);
 
 export default router;
